339cba607413afb4746ef737eef276e932d0607d
kglgan2026 <112600730+kglgan2026@users.noreply.github.com>
2024-01-16 22:39:44 +0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,15 @@
+public class Item {
+  private String name, info;
+
+  public Item(String n, String i){
+    this.name = n;
+    this.info = i;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+  public String getInfo(){
+    return this.info;
+  }
+}
