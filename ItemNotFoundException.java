c01e006aed1f06a9bbeb5a3363ceb541112148d8
kglgan2026 <112600730+kglgan2026@users.noreply.github.com>
2024-01-16 22:40:09 +0800

    Create ItemNotFoundException.java

---
+++ ItemNotFoundException.java
@@ -0,0 +1,5 @@
+public class ItemNotFoundException extends Exception {
+  public ItemNotFoundException() {
+    System.out.println("I don't know how to tell you this, but we um...kinda don't have that");
+  }
+}
