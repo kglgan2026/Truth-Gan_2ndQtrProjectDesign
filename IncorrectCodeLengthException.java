def65fda63575723340d1ed75e28009b4f0ed291
kglgan2026 <112600730+kglgan2026@users.noreply.github.com>
2024-01-16 22:39:02 +0800

    Create IncorrectCodeLengthException.java

---
+++ IncorrectCodeLengthException.java
@@ -0,0 +1,5 @@
+public class IncorrectCodeLengthException extends Exception {
+  public IncorrectCodeLengthException() {
+    System.out.println("I don't think this code is the right length");
+  }
+}
