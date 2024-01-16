b9557cbe0320ae0d4b12a3bc308e6f63d239bbbf
kglgan2026 <112600730+kglgan2026@users.noreply.github.com>
2024-01-16 22:40:34 +0800

    Create KnowNothingException.java

---
+++ KnowNothingException.java
@@ -0,0 +1,5 @@
+public class KnowNothingException extends Exception {
+  public KnowNothingException() {
+    System.out.println("Bad news, we don't actually know anything- we should probably look around first \n");
+  }
+}
